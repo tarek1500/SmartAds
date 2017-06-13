07369a0fc4f3e0a03191305eb3e545df581b6a37
Mostafa Ellethy <mostafa.ellethy93@hotmail.com>
2017-06-13 20:07:29 +0200

    fix

--- resources/views/emails/userActivation.blade.php
+++ resources/views/emails/userActivation.blade.php
@@ -94,7 +94,3 @@
 </body>
 
 </html>
-<div>
-  <a href="{{route('userActivation', $token)}}">Click Here To Activate Your Account</a>
-  <a href="{{route('userActivation', $token)}}">{{route('userActivation', $token)}}</a>
-</div>
