aeaff6428282be12e478fed5fd0873a30ff75c2c
Mostafa Ellethy <mostafa.ellethy93@hotmail.com>
2017-06-13 20:06:44 +0200

    Beautify Activation Email

--- tests/Unit/RegistrationTest.php
+++ tests/Unit/RegistrationTest.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class RegistrationTest extends TestCase
 {
     //Next ine controls database rollback
-    // use DatabaseTransactions;
+    use DatabaseTransactions;
 
     /**
      * A basic test example.
@@ -20,7 +20,7 @@ class RegistrationTest extends TestCase
     {
         $response = $this->json('POST', 'api/register', factory(\App\User::class, 1)->make()->makeVisible('password')->toArray()[0]);
         // dd(factory(\App\User::class, 1)->make()->makeVisible('password')->toArray()[0]);
-        dd($response->getContent());
+        // dd($response->getContent());
         $response->assertStatus(200)->assertJson([
         'success' => true
         ]);
