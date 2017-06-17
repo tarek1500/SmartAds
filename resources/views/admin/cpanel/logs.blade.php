51bd71022875b6bdacb2d683bec5bebe0f018ee4
Mostafa Ellethy <mostafa.ellethy93@hotmail.com>
2017-06-17 02:10:46 +0200

    logs

--- resources/views/admin/cpanel/logs.blade.php
+++ resources/views/admin/cpanel/logs.blade.php
@@ -1,22 +1,23 @@
-@extends('layouts.cpanel')
-@section('content')
+@extends('layouts.cpanel') @section('content')
 <div class="container">
   <div class="row">
     <div class="col-xs-12">
       <h1 class="page-header">Activity Logs</h1>
-      <div class="panel panel-default">
-        <div class="panel-body">
-          Section Content
-        </div>
-      </div>
+      <iframe src="{{url('/admin/logs/logs')}}"></iframe>
     </div>
     <div>
-</div>
-@endsection
-@section('scripts')
-<script>
-$(function(){
-$('#sidebar-menu').find('a.logs').addClass('active');
-});
-</script>
-@endsection
+    </div>
+    @endsection @section('scripts')
+    <script>
+      $(function() {
+        $('#sidebar-menu').find('a.logs').addClass('active');
+      });
+    </script>
+    @endsection @section('styles')
+    <style>
+      iframe {
+        width: 100%;
+        height: calc(100vh - 150px);
+      }
+    </style>
+    @endsection
